b4dc69e6a3dc033127afeafad798e5c93ec3cf0f
Anthony Radose <asradose@gmail.com>
2025-04-19 13:28:03 +0100

    Add GitHub Actions for automatic deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dev-worthy/'
+  base: '/dev-worthy/',
+  resolve: {
+    alias: {
+      // Polyfill the 'crypto' module with 'crypto-browserify'
+      crypto: path.resolve(__dirname, 'node_modules/crypto-browserify'),
+    },
+  },
 })
