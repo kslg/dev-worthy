e0a4f569d9ace6714b5b82db35e34bbdee831cbd
llancruzz <alancruzsilva@gmail.com>
2025-04-19 17:43:32 +0100

    feat: Add bootstrap icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import styles from '../styles/Footer.module.css';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <Link to="about">Credits</Link>
-    <a href="https://github.com/anthonyradose/dev-worthy" target='_blank'>GitHub</a>
+    <Link to="about">Credits <i class="bi bi-people-fill"></i></Link>
+    <a href="https://github.com/anthonyradose/dev-worthy" target='_blank'><i class="bi bi-github"></i> GitHub</a>
   </footer>
 );
 
