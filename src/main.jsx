786df95556580fb7d3794353a33356d6d99b72be
Melody <137832068+Melody-Lisa@users.noreply.github.com>
2025-04-21 06:39:30 +0000

    Remove basename from hashrouter to fix siite loading issue

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css"; // Import global styles
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/dev-worthy/">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
